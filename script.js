592158dce9c141741f4c3e32bbba6c65576ac7a7
DashkillerZ <ohpstarobo543@gmail.com>
2023-06-20 20:58:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    function showSlide() {
+        const previousSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
+        const nextSlide = (currentSlide + 1) % slides.length;
+
+        slides[currentSlide].classList.add("active");
+        slides[previousSlide].classList.remove("active");
+        slides[previousSlide].classList.add("previous");
+        slides[nextSlide].classList.add("next");
+
+        let interval = setTimeout(function() {
+            slides[previousSlide].classList.remove("previous");
+            slides[nextSlide].classList.remove("next");
+        }, 1000);
+        currentSlide = nextSlide;
+        return () => clearInterval(interval);
+    }
+
+    showSlide();
+    setInterval(showSlide, 10000); // Change slide every 5 seconds
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navbarResponsive = document.querySelector(".navbar-responsive");
+menuBtn.addEventListener("click",()=>{
+    navbarResponsive.classList.toggle("open")
+})
