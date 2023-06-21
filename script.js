6c99edec9b56af611ed4b3398fb20ed9f3b37136
DashkillerZ <ohpstarobo543@gmail.com>
2023-06-21 22:22:41 +0530

    slider update

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const slides = document.querySelectorAll(".slide");
+//     let currentSlide = 0;
 
-    function showSlide() {
-        const previousSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
-        const nextSlide = (currentSlide + 1) % slides.length;
+//     function showSlide() {
+//         const previousSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
+//         const nextSlide = (currentSlide + 1) % slides.length;
 
-        slides[currentSlide].classList.add("active");
-        slides[previousSlide].classList.remove("active");
-        slides[previousSlide].classList.add("previous");
-        slides[nextSlide].classList.add("next");
+//         slides[currentSlide].classList.add("active");
+//         slides[previousSlide].classList.remove("active");
+//         slides[previousSlide].classList.add("previous");
+//         slides[nextSlide].classList.add("next");
 
-        let interval = setTimeout(function() {
-            slides[previousSlide].classList.remove("previous");
-            slides[nextSlide].classList.remove("next");
-        }, 1000);
-        currentSlide = nextSlide;
-        return () => clearInterval(interval);
+//         let interval = setTimeout(function() {
+//             slides[previousSlide].classList.remove("previous");
+//             slides[nextSlide].classList.remove("next");
+//         }, 1000);
+//         currentSlide = nextSlide;
+//         return () => clearInterval(interval);
+//     }
+
+//     showSlide();
+//     setInterval(showSlide, 10000); // Change slide every 5 seconds
+// });
+const transitionDuration = 0.5;
+const slideInterval = 5000;
+
+const slider = document.querySelector('.carousel');
+const slides = Array.from(slider.getElementsByClassName('slide'));
+let currentIndex = 0;
+let nextIndex = currentIndex+1;
+let previousIndex = currentIndex-1;
+
+slides[currentIndex].style.opacity="1";
+function sliderAnimation() {
+    console.log(previousIndex,"-",currentIndex,"-",nextIndex)
+
+    if(currentIndex===slides.length-1){
+        currentIndex=0
+        nextIndex=currentIndex+1
+        previousIndex=currentIndex-1
+        slides[4].style.opacity ="0"
+
     }
+    else{
+        currentIndex+=1;
+        previousIndex=currentIndex-1
+        if(currentIndex===slides.length-1){
+            nextIndex=-1
 
-    showSlide();
-    setInterval(showSlide, 10000); // Change slide every 5 seconds
-});
+        }
+        else{
+            nextIndex=currentIndex+1
+        }
+    }
+    slides[currentIndex].style.opacity = "1";
+    slides[previousIndex].style.opacity ="0"
+}
+
+setInterval(sliderAnimation, slideInterval)
+
+
+
+
 
 const menuBtn = document.querySelector(".menu-btn");
 const navbarResponsive = document.querySelector(".navbar-responsive");
